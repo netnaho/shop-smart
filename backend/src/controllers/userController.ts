9972b36b8b711228c79647553e9e5ad8565a2111
netnaho <87896136+netnaho@users.noreply.github.com>
2024-12-10 10:18:56 +0300

    user related files

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import ClientModel from "../schemas/userSchema";
+import CategoryModel from "../schemas/categorySchema";
+import ProductModel from "../schemas/productSchema";
+import { getAccessToken } from "../utils/tokens";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface AuthenticatedRequest extends Request {
+  user?: Object | JwtPayload;
+}
 
 export const registerUser = async (req: Request, res: Response) => {
   const { fullname, age, location, phone, email, password } = req.body;
@@ -33,8 +41,58 @@ export const loginUser = async (req: Request, res: Response) => {
       if (password !== userPassword) {
         throw new Error("Invalid password");
       }
-    res.status(200).json({ message: "login success" });
+    const token = await getAccessToken({
+      user: user,
+    }); // get access token
+    console.log("token is:", token);
+    res.cookie("token", token, {
+      httpOnly: true, // only from http not from the javascript
+      secure: false,
+      // sameSite: "none", // check wether it is in production or development
+      maxAge: 7 * 24 * 60 * 60 * 1000, // token live only for 24 hours
+    });
+    res.status(200).json({ message: "login success", token: token });
   } catch (error: any) {
+    console.log(error);
     res.status(404).json({ error: error.message });
+  }
+};
+
+export const getCategories = async (req: Request, res: Response) => {
+  try {
+    const categories = await CategoryModel.find({});
+    res.status(200).json(categories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
+};
+
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await ProductModel.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
+};
+
+export const topUpWallet = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+};
+
+export const getUserInfo = async (req: AuthenticatedRequest, res: Response) => {
+  const user = req.user;
+  try {
+    console.log(user);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
   }
 };
--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,6 +1,17 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userController";
+import {
+  getCategories,
+  getProducts,
+  getUserInfo,
+  loginUser,
+  registerUser,
+} from "../controllers/userController";
+import { userAuthenticateAccessToken } from "../middleware/authenticateAccesstoken";
 export const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/get-categories", userAuthenticateAccessToken, getCategories);
+userRouter.get("/get-products", userAuthenticateAccessToken, getProducts);
+userRouter.post("/top-up-wallet");
+userRouter.get("/user-info", userAuthenticateAccessToken, getUserInfo);
